945fbc1fac642331c229f8f0ead6eb33a0998bb0
SupahXYT <josuefirelol@gmail.com>
2021-10-07 15:40:07 -0700

    done

--- Path.py
+++ Path.py
@@ -1,3 +1,4 @@
+from math import inf
 
 class path:
     def __init__(self, adj):
@@ -5,8 +6,10 @@ class path:
         self.adj = adj
 
     def distance(self):
+        if self.path[-1] == None:
+            return inf
         distance = 0
-        for i in range(0, len(self.path) - 1):
+        for i in range(len(self.path) - 1):
             f = i
             t = i+1
             distance += self.adj[self.path[f]][self.path[t]]
--- heaprunner.py
+++ heaprunner.py
@@ -1,5 +1,6 @@
 import Heap, Path
 from random import randrange
+from math import inf
 
 heap = Heap.min_heap()
 
@@ -7,7 +8,7 @@ for i in range(1, 7):
     random = randrange(1, 10)
     heap.add(random)
 
-print(heap.heap)
+# print(heap.heap)
 
 def new_graph(v):
     # I really wish I could do 
@@ -25,15 +26,21 @@ def new_graph(v):
 # graph = new_graph(5)
 
 graph = [[0, 3, 1, 1],
-         [3, 0, 0, 3],
-         [2, 0, 0, 0],
-         [1, 3, 0, 0]]
+         [3, 0, inf, 3],
+         [2, inf, 0, inf],
+         [1, 3, inf, 0]]
+
+# graph = [[0, inf, inf],
+#          [inf, 0, 2],
+#          [inf, 2, 0]]
 
 # given two vertices in the graph G this function returns 
 # the shortest path between those two vertices with a path object
 
 print(graph)
 
+# returns path object containing shortest path if found 
+# or returns new path instance representing no path
 def dijkstra(start, end):
     heap = Heap.path_heap()
     visited = []
@@ -42,23 +49,25 @@ def dijkstra(start, end):
     startingpath = Path.path(graph)
     startingpath.add_vertex(start)
     heap.add(startingpath)
-    
-    while True:
+    # While not dead end
+    while len(heap.heap) > 0:
         path = heap.remove()
         current = path.last()
         if current == end:
-            return path.path
+            return path
         # for every other node
         for i in range(len(graph)):
             # check if neighbor
-            if graph[current][i] > 0 and not contains(visited, i):
-                print(i)
-                print(path.path)
+            if graph[current][i] > 0 and graph[current][i] < inf and not contains(visited, i):
                 p = Path.path(graph)
                 p.path = [i for i in path.path]
                 p.add_vertex(i)
                 heap.add(p)
         visited.append(current)
+
+    fin = Path.path(graph)
+    fin.add_vertex(None)
+    return fin
 
 def contains(list, n):
     for i in range(0, len(list)):
@@ -66,13 +75,8 @@ def contains(list, n):
             return True
     return False
         
-shortest = dijkstra(2, 1)
-print(f'path: {shortest}')
+f, t = 2, 1
+shortest = dijkstra(f, t)
+print(f'shortest path from {f} to {t}: {shortest.path}')
+print(f'distance: {shortest.distance()}')
 
-# def distance(path):
-#     distance = 0
-#     for i in range(0, len(path) - 1):
-#         f = path[i]
-#         t = path[i+1]
-#         distance += graph[f][t]
-#     return distance 
